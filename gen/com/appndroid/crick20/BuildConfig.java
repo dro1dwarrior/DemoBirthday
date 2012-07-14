6709a5a869a1c7f7b8c6d23c12cf0b62a4740381
Pankaj-T <p.thadani8@gmail.com>
2012-07-14 12:19:26 +0530

    removed gen files

--- gen/com/appndroid/crick20/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.appndroid.crick20;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- src/com/appndroid/crick20/Hello.java
+++ src/com/appndroid/crick20/Hello.java
@@ -7,6 +7,7 @@ public class Hello extends Activity{
 	 @Override
 	    public void onCreate( Bundle savedInstanceState )
 	    {
+		//
 	        super.onCreate( savedInstanceState );
 	        setContentView( R.layout.hello);
 	    }
