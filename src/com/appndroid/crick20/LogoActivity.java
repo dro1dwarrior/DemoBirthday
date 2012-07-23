70c614f22918cc8c4c739be91520d59556f1d353
Pramod Warrier <pwarrier123@gmail.com>
2012-07-23 18:08:03 +0530

    changes in thread login :D

--- src/com/appndroid/crick20/LogoActivity.java
+++ src/com/appndroid/crick20/LogoActivity.java
@@ -1,22 +1,20 @@
 package com.appndroid.crick20;
 
-import java.util.Timer;
-
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.Window;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 
 public class LogoActivity extends Activity
 {
 
-    private final Handler mHandler = new Handler();
     private Drawable mCurrentDrawable;
-    private Timer myTimer;
     int count = 0;
     private SeekBar seekBar;
     Thread displayImage;
@@ -26,6 +24,7 @@ public class LogoActivity extends Activity
     public void onCreate( Bundle savedInstanceState )
     {
         super.onCreate( savedInstanceState );
+        this.requestWindowFeature( Window.FEATURE_NO_TITLE );
         setContentView( R.layout.main );
         mCurrentDrawable = getResources().getDrawable( R.drawable.clip );
         findViewById( R.id.testView ).setBackgroundDrawable( mCurrentDrawable );
@@ -50,40 +49,51 @@ public class LogoActivity extends Activity
 
         } );
 
-        displayImage = new Thread()
-        {
-            public void run()
-            {
-                do
-                {
-                    count += 3;
-                    seekBar.setProgress( count );
-                }
-                while( count < 100000 );
+        showSplash( this, null );
 
-                if( count >= 100000 )
-                {
-                    count = 0;
-                    try
-                    {
-                        displayImage.sleep( 100 );
-                    }
-                    catch( Exception e )
-                    {
-                        // TODO: handle exception
-                        e.printStackTrace();
-                    }
-                    Intent intent;
-                    intent = new Intent( getApplicationContext(), com.appndroid.crick20.FlagsActivity.class );
-                    intent.setFlags( Intent.FLAG_ACTIVITY_NEW_TASK );
-                    intent.addFlags( Intent.FLAG_ACTIVITY_CLEAR_TOP );
-                    LogoActivity.this.finish();
-                    startActivity( intent );
-                }
-
-            }
-        };
-        displayImage.start();
     }
 
+    private void showSplash( final Context context, final Intent intentShortCut )
+    {
+        // The thread to wait for splash screen events
+        displayImage = new Thread()
+        {
+            @Override
+            public void run()
+            {
+                try
+                {
+                    synchronized( this )
+                    {
+                        // Wait given period of time or exit on touch
+                        wait( 100 );
+                    }
+                    do
+                    {
+                        count += 2;
+                        seekBar.setProgress( count / 2 );
+                    }
+                    while( count < 60000 );
+
+                    if( count >= 60000 )
+                    {
+                        count = 0;
+                        Intent intent;
+                        intent = new Intent( getApplicationContext(), com.appndroid.crick20.FlagsActivity.class );
+                        intent.setFlags( Intent.FLAG_ACTIVITY_NEW_TASK );
+                        LogoActivity.this.finish();
+                        startActivity( intent );
+                    }
+                }
+                catch( Exception ex )
+                {
+                    ex.printStackTrace();
+                }
+
+                finish();
+            }
+        };
+
+        displayImage.start();
+    }
 }
