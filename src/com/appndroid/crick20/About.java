2ee5aa9f2dbc9f0198276c7b98f24239bf340fe1
Pramod Warrier <pwarrier123@gmail.com>
2012-08-31 12:30:26 +0530

    added about and some changes in grouptab

---
+++ src/com/appndroid/crick20/About.java
@@ -0,0 +1,80 @@
+package com.appndroid.crick20;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class About extends Activity
+{
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState )
+    {
+        // TODO Auto-generated method stub
+        super.onCreate( savedInstanceState );
+        requestWindowFeature( Window.FEATURE_NO_TITLE );
+        setContentView( R.layout.about );
+
+        final Button btnSubmit = (Button) findViewById( R.id.btnSubmit );
+        btnSubmit.setEnabled( false );
+        final EditText etMessage = (EditText) findViewById( R.id.etMessage );
+        etMessage.addTextChangedListener( new TextWatcher()
+        {
+
+            @Override
+            public void afterTextChanged( Editable s )
+            {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void beforeTextChanged( CharSequence s, int start, int count, int after )
+            {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onTextChanged( CharSequence s, int start, int before, int count )
+            {
+                // TODO Auto-generated method stub
+                if( etMessage.getText().length() > 9 )
+                    btnSubmit.setEnabled( true );
+            }
+
+        } );
+
+        btnSubmit.setOnClickListener( new OnClickListener()
+        {
+
+            @Override
+            public void onClick( View v )
+            {
+                // TODO Auto-generated method stub
+                Intent i = new Intent( Intent.ACTION_SEND );
+                i.setType( "message/rfc822" );
+                i.putExtra( Intent.EXTRA_EMAIL, new String[] { "ipltwenty12@gmail.com" } );
+                i.putExtra( Intent.EXTRA_SUBJECT, "feedback" );
+                i.putExtra( Intent.EXTRA_TEXT, etMessage.getText().toString().trim() );
+                try
+                {
+                    startActivity( Intent.createChooser( i, "Send mail..." ) );
+                }
+                catch( Exception ex )
+                {
+                    ex.printStackTrace();
+                    Toast.makeText( About.this, "There are no email clients installed.", Toast.LENGTH_SHORT ).show();
+                }
+            }
+        } );
+    }
+}
