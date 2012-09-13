47c495feba33a1df2199c1b27bdc96580e9d5f48
Pramod Warrier <pwarrier123@gmail.com>
2012-09-13 10:58:18 +0530

    new files

--- src/com/appndroid/crick20/HomeScreen.java
+++ src/com/appndroid/crick20/HomeScreen.java
@@ -7,79 +7,81 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
-public class HomeScreen extends Activity{
-	
-	ImageView scheduleClick, pointsTableClick, aboutClick, settingsClick;
-	Context mcontext;
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		super.onCreate(savedInstanceState);
-		setContentView( R.layout.home);
-		
-		mcontext=this;
-		aboutClick = (ImageView) findViewById(R.id.about_img);
-		settingsClick = (ImageView) findViewById(R.id.settings_img);
-		scheduleClick=(ImageView)findViewById(R.id.sch_img);
-		pointsTableClick = (ImageView) findViewById(R.id.standings_img);
-		
-		scheduleClick.setOnClickListener(new View.OnClickListener() {
+public class HomeScreen extends Activity
+{
 
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
+    ImageView scheduleClick, pointsTableClick, aboutClick, settingsClick;
+    Context mcontext;
 
-				    Intent schIntent = new Intent(mcontext,
-							Schedule.class);
-					schIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-					schIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-					mcontext.startActivity(schIntent);
-					
-				
-			}
-		});
-		
-		pointsTableClick.setOnClickListener(new View.OnClickListener() {
+    @Override
+    protected void onCreate( Bundle savedInstanceState )
+    {
+        // TODO Auto-generated method stub
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.home_new );
 
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-
-				    Intent schIntent = new Intent(mcontext,
-							GroupTab.class);
-					schIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-					schIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-					mcontext.startActivity(schIntent);
-					
-				
-			}
-		});
-		
-		settingsClick.setOnClickListener(new View.OnClickListener() {
-			
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				Intent schIntent = new Intent(mcontext,
-						Settings.class);
-				schIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-				schIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-				mcontext.startActivity(schIntent);
-				
-			}
-		});
-		
-		aboutClick.setOnClickListener(new View.OnClickListener() {
-			
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				Intent schIntent = new Intent(mcontext,
-						About.class);
-				schIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-				schIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-				mcontext.startActivity(schIntent);
-				
-			}
-		});
-		
-	}
+        mcontext = this;
+        // aboutClick = (ImageView) findViewById(R.id.about_img);
+        // settingsClick = (ImageView) findViewById(R.id.settings_img);
+        // scheduleClick=(ImageView)findViewById(R.id.sch_img);
+        // pointsTableClick = (ImageView) findViewById(R.id.standings_img);
+        //
+        // scheduleClick.setOnClickListener(new View.OnClickListener() {
+        //
+        // public void onClick(View v) {
+        // // TODO Auto-generated method stub
+        //
+        // Intent schIntent = new Intent(mcontext,
+        // Schedule.class);
+        // schIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        // schIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        // mcontext.startActivity(schIntent);
+        //
+        //
+        // }
+        // });
+        //
+        // pointsTableClick.setOnClickListener(new View.OnClickListener() {
+        //
+        // public void onClick(View v) {
+        // // TODO Auto-generated method stub
+        //
+        // Intent schIntent = new Intent(mcontext,
+        // GroupTab.class);
+        // schIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        // schIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        // mcontext.startActivity(schIntent);
+        //
+        //
+        // }
+        // });
+        //
+        // settingsClick.setOnClickListener(new View.OnClickListener() {
+        //
+        // public void onClick(View v) {
+        // // TODO Auto-generated method stub
+        // Intent schIntent = new Intent(mcontext,
+        // Settings.class);
+        // schIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        // schIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        // mcontext.startActivity(schIntent);
+        //
+        // }
+        // });
+        //
+        // aboutClick.setOnClickListener(new View.OnClickListener() {
+        //
+        // public void onClick(View v) {
+        // // TODO Auto-generated method stub
+        // Intent schIntent = new Intent(mcontext,
+        // About.class);
+        // schIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        // schIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        // mcontext.startActivity(schIntent);
+        //
+        // }
+        // });
+        //
+    }
 
 }
