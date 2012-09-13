61bb5af0ba02c8fd7d08f56eb2fc412096762297
Pramod Warrier <pwarrier123@gmail.com>
2012-09-13 18:31:51 +0530

    changes in manifest

--- src/com/appndroid/crick20/LogoActivity.java
+++ src/com/appndroid/crick20/LogoActivity.java
@@ -10,117 +10,153 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Window;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 
-public class LogoActivity extends Activity {
+import com.google.android.gcm.GCMRegistrar;
 
-	private final Handler mHandler = new Handler();
-	private Drawable mCurrentDrawable;
-	private Timer myTimer;
-	int count;
-	private SeekBar seekBar;
+public class LogoActivity extends Activity
+{
 
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-		setContentView(R.layout.main);
-		mCurrentDrawable = getResources().getDrawable(R.drawable.clip);
-		findViewById(R.id.testView).setBackgroundDrawable(mCurrentDrawable);
+    private final Handler mHandler = new Handler();
+    private Drawable mCurrentDrawable;
+    private Timer myTimer;
+    int count;
+    private SeekBar seekBar;
 
-		Calendar c = Calendar.getInstance();
-		int milli = c.getTimeZone().getOffset(TimeZone.LONG);
-		SharedPreferences sp = PreferenceManager
-				.getDefaultSharedPreferences(this);
-		Editor edit = sp.edit();
-		edit.putInt("offset", milli);
-		edit.commit();
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate( Bundle savedInstanceState )
+    {
+        super.onCreate( savedInstanceState );
+        this.requestWindowFeature( Window.FEATURE_NO_TITLE );
+        setContentView( R.layout.main );
+        mCurrentDrawable = getResources().getDrawable( R.drawable.clip );
+        findViewById( R.id.testView ).setBackgroundDrawable( mCurrentDrawable );
 
-		seekBar = (SeekBar) findViewById(R.id.seekBar);
-		seekBar.setMax(10000);
-		seekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+        // GCM Start
+        int build_version = Integer.parseInt( Build.VERSION.SDK );
+        if( build_version >= 8 )
+        {
+            GCMRegistrar.checkDevice( this );
+            GCMRegistrar.checkManifest( this );
+            final String regId = GCMRegistrar.getRegistrationId( this );
+            if( regId.equals( "" ) )
+            {
+                // GCMRegistrar.register( this, "GCM_SENDER_ID" );
+                Log.d( "LogoActivity-GCMRegister()", "GCM register call check passed" );
+            }
+            else
+            {
+                Log.d( "LogoActivity-GCMRegister()", "GCM registered Id" + regId );
+            }
+        }
+        // GCM End
 
-			public void onStopTrackingTouch(SeekBar seekBar) {
-			}
+        Calendar c = Calendar.getInstance();
+        int milli = c.getTimeZone().getOffset( TimeZone.LONG );
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences( this );
+        Editor edit = sp.edit();
+        edit.putInt( "offset", milli );
+        edit.commit();
 
-			public void onStartTrackingTouch(SeekBar seekBar) {
-			}
+        seekBar = (SeekBar) findViewById( R.id.seekBar );
+        seekBar.setMax( 10000 );
+        seekBar.setOnSeekBarChangeListener( new OnSeekBarChangeListener()
+        {
 
-			public void onProgressChanged(SeekBar seekBar, int progress,
-					boolean fromUser) {
-				mCurrentDrawable.setLevel(progress);
-			}
+            public void onStopTrackingTouch( SeekBar seekBar )
+            {
+            }
 
-		});
+            public void onStartTrackingTouch( SeekBar seekBar )
+            {
+            }
 
-		myTimer = new Timer();
-		myTimer.schedule(new TimerTask() {
-			@Override
-			public void run() {
-				TimerMethod();
-			}
+            public void onProgressChanged( SeekBar seekBar, int progress, boolean fromUser )
+            {
+                mCurrentDrawable.setLevel( progress );
+            }
 
-		}, 0, 100);
-	}
+        } );
 
-	private void TimerMethod() {
-		// This method is called directly by the timer
-		// and runs in the same thread as the timer.
+        myTimer = new Timer();
+        myTimer.schedule( new TimerTask()
+        {
+            @Override
+            public void run()
+            {
+                TimerMethod();
+            }
 
-		// We call the method that will work with the UI
-		// through the runOnUiThread method.
-		this.runOnUiThread(Timer_Tick);
-	}
+        }, 0, 100 );
+    }
 
-	private Runnable Timer_Tick = new Runnable() {
-		public void run() {
+    private void TimerMethod()
+    {
+        // This method is called directly by the timer
+        // and runs in the same thread as the timer.
 
-			// This method runs in the same thread as the UI.
+        // We call the method that will work with the UI
+        // through the runOnUiThread method.
+        this.runOnUiThread( Timer_Tick );
+    }
 
-			// Do something to the UI thread here
-			count += 400;
-			seekBar.setProgress(count);
+    private Runnable Timer_Tick = new Runnable()
+    {
+        public void run()
+        {
 
-			if (count > 10000) {
-				try {
-					Thread.sleep(100);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				Intent intent;
-				intent = new Intent(getApplicationContext(),
-						com.appndroid.crick20.FlagsActivity.class);
-				intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-				LogoActivity.this.finish();
-				myTimer.cancel();
-				startActivity(intent);
-				finish();
-			}
+            // This method runs in the same thread as the UI.
 
-		}
-	};
-	
-	@Override
-	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		if (keyCode == KeyEvent.KEYCODE_BACK)
-			{
-				myTimer.cancel();
-				finish();
-	            int pid = android.os.Process.myPid(); 
-	            android.os.Process.killProcess( pid );
-	            System.exit(0);
-	            return true;  
-				
-			}
-		//return super.onKeyUp(keyCode, event);
-		return false;
-	}
+            // Do something to the UI thread here
+            count += 400;
+            seekBar.setProgress( count );
+
+            if( count > 10000 )
+            {
+                try
+                {
+                    Thread.sleep( 100 );
+                }
+                catch( InterruptedException e )
+                {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                Intent intent;
+                intent = new Intent( getApplicationContext(), com.appndroid.crick20.FlagsActivity.class );
+                intent.setFlags( Intent.FLAG_ACTIVITY_NEW_TASK );
+                LogoActivity.this.finish();
+                myTimer.cancel();
+                startActivity( intent );
+                finish();
+            }
+
+        }
+    };
+
+    @Override
+    public boolean onKeyDown( int keyCode, KeyEvent event )
+    {
+        if( keyCode == KeyEvent.KEYCODE_BACK )
+        {
+            myTimer.cancel();
+            finish();
+            int pid = android.os.Process.myPid();
+            android.os.Process.killProcess( pid );
+            System.exit( 0 );
+            return true;
+
+        }
+        // return super.onKeyUp(keyCode, event);
+        return false;
+    }
 }
